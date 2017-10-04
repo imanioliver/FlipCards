3dac000a9a4fae041c5eb5691fedbaf9dd631b63
Imani Oliver <isaac@ihardy.co>
2017-10-04 11:07:07 -0400

    buttons incrementing correctly, adds css for a cleaner look

--- public/quizScore.js
+++ public/quizScore.js
@@ -2,16 +2,61 @@ console.log("workinggggg");
 
 let correctButton= document.getElementById("correctButton");
 let incorrectButton= document.getElementById("incorrectButton");
-
-let flashCards = document.getElementsByClassName("flipper")
-
+let scoreDisplay= document.getElementById("scoreDisplay");
+let flashCards = document.getElementsByClassName("flashCards");
+let quizComplete = document.getElementById('quizComplete');
 let total = flashCards.length;
 
 let score = 0
+let attempts=0
+console.log("score: ", score);
 
-console.log(total);
+console.log("total: ", total);
+console.log("attempts: ", attempts);
 
-correctButton.addEventLister('click', (event)=>{
+
+//
+// if(attempts!== 0 &&  ){
+//
+// }
+
+
+correctButton.addEventListener('click', (event)=>{
     event.preventDefault();
 
+    if (attempts != total){
+        score++;
+        attempts++;
+        scoreDisplay.innerHTML = `Your Score: ${score}/${total}`
+
+    }
+    else {
+        alert(scoreDisplay.innerHTML = `Your FINAL Score: ${score}/${total}`)
+
+
+    }
+
+    console.log("score after increment: ", score);
+    console.log("attempts after increment: ", attempts);
+
 })
+
+incorrectButton.addEventListener('click', (event)=>{
+    event.preventDefault();
+
+    if (attempts != total){
+        attempts++;
+        scoreDisplay.innerHTML = `Your Score: ${score}/${total}`
+    }
+    else{
+        alert(scoreDisplay.innerHTML = `QUIZ FINISHED! Your FINAL Score: ${score}/${total}`)
+    }
+    console.log("score after decrement: ", score);
+    console.log("attempts after decrement: ", attempts);
+
+})
+// scoreDisplay.innerHTML+=`<diYour Score: ${score}/${total}</div>`
+
+if (attempts!==0 && attempts===score){
+    quizComplete.innerHTML+=`your FINAL score!`
+}
