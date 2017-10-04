5876f0911aca0d7461abf460e65efe91e6a1e8c3
Imani Oliver <isaac@ihardy.co>
2017-10-03 20:11:16 -0400

    successfully adds the number of cards in a deck in the console

--- public/quizScore.js
+++ public/quizScore.js
@@ -1,1 +1,17 @@
 console.log("workinggggg");
+
+let correctButton= document.getElementById("correctButton");
+let incorrectButton= document.getElementById("incorrectButton");
+
+let flashCards = document.getElementsByClassName("flipper")
+
+let total = flashCards.length;
+
+let score = 0
+
+console.log(total);
+
+correctButton.addEventLister('click', (event)=>{
+    event.preventDefault();
+
+})
